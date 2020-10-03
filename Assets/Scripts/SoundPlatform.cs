744dea0de2b15b518133c68d204d62653b1f1317
Arrigo <arrigo.paterno@gmx.de>
2020-10-03 12:18:50 +0200

    Added collider with callback

--- Assets/Scripts/SoundPlatform.cs
+++ Assets/Scripts/SoundPlatform.cs
@@ -1,26 +1,9 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class SoundPlatform : MonoBehaviour
 {
-    [SerializeField] private Collider2D left, right, center;
-
-
-    private void OnCollisionEnter2D(Collision2D other)
+    public void ColliderHit(Collider2D other)
     {
-        if (other.collider == left)
-        {
-            Debug.LogError($"{other.collider.name}");
-        }
-        else if (other.collider == right)
-        {
-            Debug.LogError($"{other.collider.name}");
-        }
-        else if (other.collider == center)
-        {
-            Debug.LogError($"{other.collider.name}"); 
-        }
+        Debug.LogError($"{other.name}"); 
     }
 }
---
+++ Assets/Scripts/SoundPlatformCollider.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundPlatformCollider : MonoBehaviour
+{
+    [SerializeField] private SoundPlatform platform;
+    [SerializeField] private new Collider2D collider2D;
+    
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        platform.ColliderHit(collider2D);
+    }
+}
