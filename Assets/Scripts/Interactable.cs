28da2df8796b07155c4234fe8eabe3d4e853820f
Arrigo <arrigo.paterno@gmx.de>
2020-10-03 14:40:54 +0200

    Added prefabs and moved draggable code to other file

---
+++ Assets/Scripts/Interactable.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Interactable : MonoBehaviour,  IPointerClickHandler, IDragHandler, IBeginDragHandler
+{
+    private Vector2 _lastGrabWorldPos;
+    private GameObject _grabbedObject;
+
+    [SerializeField] private GameObject leftGo, rightGo, centerGo;
+
+    private Camera _camera;
+
+    private void Awake()
+    {
+        _camera = Camera.main;
+    }
+
+    private Vector2 Left
+    {
+        get => leftGo.transform.position;
+        set => SetPosition(value, Right);
+    }
+
+    private Vector2 Right
+    {
+        get => rightGo.transform.position;
+        set => SetPosition(Left, value);
+    }
+
+    private void SetPosition(Vector2 left, Vector2 right)
+    {
+        Transform tr = transform;
+        
+        var delta = right - left;
+        tr.position = (left + right) / 2;
+       
+        tr.rotation = Quaternion.FromToRotation(Vector3.right, delta);
+        tr.localScale = new Vector3(delta.magnitude, tr.localScale.y, 1);
+    }
+
+    public void ColliderHit(Collider2D other)
+    {
+        Debug.LogError($"{other.name}");
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (this && eventData.button == PointerEventData.InputButton.Right)
+            Destroy(gameObject);
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        _grabbedObject = eventData.pointerEnter.gameObject;
+        _lastGrabWorldPos = _camera.ScreenToWorldPoint(eventData.position);
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        var newPos = (Vector2) _camera.ScreenToWorldPoint(eventData.position);
+        var delta = newPos - _lastGrabWorldPos;
+        _lastGrabWorldPos = newPos;
+        
+        if (_grabbedObject == centerGo)
+        {
+            transform.position += new Vector3(delta.x, delta.y, 0);
+        }
+        else if (_grabbedObject == rightGo)
+        {
+            Right += delta;
+        }
+        else if (_grabbedObject == leftGo)
+        {
+            Left += delta;
+        }
+    }
+}
--- Assets/Scripts/Platform.cs
+++ Assets/Scripts/Platform.cs
@@ -1,71 +1,11 @@
-﻿using Unity.Mathematics;
-using UnityEngine;
-using UnityEngine.EventSystems;
+﻿using UnityEngine;
 
-public class Platform : MonoBehaviour, IPointerClickHandler, IDragHandler, IBeginDragHandler
+
+public class Platform : MonoBehaviour
 {
-    private Vector2 lastGrabWorldPos;
-    private GameObject grabbedObject;
-    private bool grabbingLeft;
-
-    [SerializeField] private GameObject leftGo, rightGo, centerGo;
-
-    private Vector2 left
-    {
-        get => leftGo.transform.position;
-        set => SetPosition(value, right);
-    }
-
-    private Vector2 right
-    {
-        get => rightGo.transform.position;
-        set => SetPosition(left, value);
-    }
-
-    private void SetPosition(Vector2 left, Vector2 right)
-    {
-        var delta = right - left;
-        transform.position = (left + right) / 2;
-        transform.rotation = Quaternion.FromToRotation(Vector3.right, delta);
-        transform.localScale = new Vector3(delta.magnitude, transform.localScale.y, 1);
-    }
-
-    private Vector2 center => transform.position;
-
+    
     public void ColliderHit(Collider2D other)
     {
         Debug.LogError($"{other.name}");
-    }
-
-    public void OnPointerClick(PointerEventData eventData)
-    {
-        if (this && eventData.button == PointerEventData.InputButton.Right)
-            Destroy(gameObject);
-    }
-
-    public void OnBeginDrag(PointerEventData eventData)
-    {
-        Debug.Log(eventData.pointerEnter.gameObject.name);
-        grabbedObject = eventData.pointerEnter.gameObject;
-        lastGrabWorldPos = Camera.main.ScreenToWorldPoint(eventData.position);
-    }
-
-    public void OnDrag(PointerEventData eventData)
-    {
-        var newPos = (Vector2) Camera.main.ScreenToWorldPoint(eventData.position);
-        var delta = newPos - lastGrabWorldPos;
-        lastGrabWorldPos = newPos;
-        if (grabbedObject == centerGo)
-        {
-            transform.position += new Vector3(delta.x, delta.y, 0);
-        }
-        else if (grabbedObject == rightGo)
-        {
-            right += delta;
-        }
-        else if (grabbedObject == leftGo)
-        {
-            left += delta;
-        }
     }
 }
