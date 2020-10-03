5ac440d6ea9ecb4cefed44f62a7af234dfdd6be2
Jonas Kunze <kunze.jonas@gmail.com>
2020-10-03 13:28:34 +0200

    Gabbing Center works

---
+++ Assets/Scripts/Platform.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Platform : MonoBehaviour, IPointerClickHandler, IDragHandler, IBeginDragHandler
+{
+    private Vector2 lastGrabWorldPos;
+    private GameObject grabbedObject;
+    private bool grabbingLeft;
+
+    [SerializeField] private GameObject leftGo, rightGo, centerGo;
+
+    private Vector2 left => leftGo.transform.position;
+    private Vector2 right => rightGo.transform.position;
+    private Vector2 center => transform.position;
+
+    public void ColliderHit(Collider2D other)
+    {
+        Debug.LogError($"{other.name}");
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (this && eventData.button == PointerEventData.InputButton.Right)
+            Destroy(gameObject);
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        Debug.Log(eventData.pointerEnter.gameObject.name);
+        grabbedObject = eventData.pointerEnter.gameObject;
+        lastGrabWorldPos = Camera.main.ScreenToWorldPoint(eventData.position);
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        if (grabbedObject == centerGo)
+        {
+            var newPos = (Vector2) Camera.main.ScreenToWorldPoint(eventData.position);
+            var delta = newPos - lastGrabWorldPos;
+            lastGrabWorldPos = newPos;
+            transform.position += new Vector3(delta.x, delta.y, 0);
+        }
+    }
+}
