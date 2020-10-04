160fef506edd268f44240e6da10d1c78480d8c9a
Arrigo <arrigo.paterno@gmx.de>
2020-10-04 12:31:21 +0200

    Stuff14?

--- Assets/Scripts/BallSpawnTrigger.cs
+++ Assets/Scripts/BallSpawnTrigger.cs
@@ -25,39 +25,19 @@ public class BallSpawnTrigger : SerializableObject, IInteractable
         }
     }
 
-    public void OnCreationStart(Transform parent, Vector3 startPosition)
-    {
-        var tr = GetTransform();
+    public void OnCreationStart(Transform parent, Vector3 startPosition) => GetInteractable().OnCreationStart(parent, startPosition);
 
-        tr.SetParent(parent);
-        tr.position = startPosition;
-        tr.localScale = new Vector3(0, tr.localScale.y, 0);
-        GetInteractable().SetActive(false);
-    }
+    public void OnCreationFinish() => GetInteractable().OnCreationFinish();
 
-    public void OnCreationFinish()
-    {
-        if (transform.localScale.x > .3)
-            _interactable.SetActive(true);
-        else
-            Destroy(gameObject);
-    }
-
-    public void OnCreationUpdate(Vector3 newPosition, Vector3 startPosition)
-    {
-        var dir = newPosition - startPosition;
-        var center = (startPosition + newPosition) * 0.5f;
-
-        var position = center;
-        var rotation = Quaternion.FromToRotation(Vector3.right, dir);
-
-        transform.position = position;
-        transform.rotation = rotation;
-        transform.localScale = new Vector3(dir.magnitude, transform.localScale.y, 0);
-    }
+    public void OnCreationUpdate(Vector3 newPosition, Vector3 startPosition) => GetInteractable().OnCreationUpdate(newPosition, startPosition);
 
     public Transform GetTransform() => transform;
-    public Interactable GetInteractable() => _interactable;
+    public Interactable GetInteractable()
+    {
+        if (_interactable == null)
+            _interactable = GetComponentInChildren<Interactable>();
+        return _interactable;
+    }
 
     public override BlueprintIndex GetBlueprintIndex()    {
         return BlueprintIndex.SpawnTrigger;
--- Assets/Scripts/Interactable.cs
+++ Assets/Scripts/Interactable.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class Interactable : MonoBehaviour,  IPointerClickHandler, IDragHandler, IBeginDragHandler, IEndDragHandler
+public class Interactable : MonoBehaviour, IInteractable, IPointerClickHandler, IDragHandler, IBeginDragHandler, IEndDragHandler
 {
     private Vector2 _lastGrabWorldPos;
     private GameObject _grabbedObject;
@@ -9,25 +9,32 @@ public class Interactable : MonoBehaviour,  IPointerClickHandler, IDragHandler, 
     public static bool IsDragging;
 
     [SerializeField] private GameObject leftGo, rightGo, centerGo;
-
+    [SerializeField] private SpriteRenderer spriteRenderer = null;
+    
     private Camera _camera;
-    private Collider2D[] _colliders;
+    private BoxCollider2D _leftCollider, _rightCollider, _centerCollider;
 
     private void Awake()
     {
         _camera = Camera.main;
-        _colliders = GetComponentsInChildren<Collider2D>();
+      
+        _leftCollider = leftGo.GetComponent<BoxCollider2D>();
+        _rightCollider = rightGo.GetComponent<BoxCollider2D>();
+        _centerCollider = centerGo.GetComponent<BoxCollider2D>();
+
+        Debug.Assert(spriteRenderer != null);
+        spriteRenderer.drawMode = SpriteDrawMode.Sliced;
     }
 
     private Vector2 Left
     {
-        get => leftGo.transform.position;
+        get => transform.position - transform.right * spriteRenderer.size.x * 0.5f;
         set => SetPosition(value, Right);
     }
 
     private Vector2 Right
     {
-        get => rightGo.transform.position;
+        get => transform.position + transform.right * spriteRenderer.size.x * 0.5f;
         set => SetPosition(Left, value);
     }
 
@@ -39,15 +46,34 @@ public class Interactable : MonoBehaviour,  IPointerClickHandler, IDragHandler, 
         tr.position = (left + right) / 2;
        
         tr.rotation = Quaternion.FromToRotation(Vector3.right, delta);
-        tr.localScale = new Vector3(delta.magnitude, tr.localScale.y, 1);
+        SetScale(delta.magnitude);
+    }
+
+    private void SetScale(float scaleFactor)
+    {
+        spriteRenderer.size = new Vector2(scaleFactor, spriteRenderer.size.y);
+
+        var sizeX = (scaleFactor * 0.5f);
+        
+        var leftColliderPosition = -Vector3.right * (sizeX - (0.125f * scaleFactor));
+        var rightColliderPosition = Vector3.right * (sizeX - (0.125f * scaleFactor));
+
+        leftGo.transform.localPosition = leftColliderPosition;
+        rightGo.transform.localPosition = rightColliderPosition;
+        centerGo.transform.position = transform.position;
+        
+        _leftCollider.size = new Vector2(0.25f * scaleFactor, _leftCollider.size.y);
+        _rightCollider.size = new Vector2( 0.25f * scaleFactor, _rightCollider.size.y);
+        _centerCollider.size = new Vector2( 0.5f * scaleFactor, _centerCollider.size.y);
     }
 
     public void SetActive(bool value)
     {
         enabled = value;
-        
-        foreach (var col in _colliders)
-            col.enabled = enabled;
+
+        _leftCollider.enabled = value;
+        _rightCollider.enabled = value;
+        _centerCollider.enabled = value;
     }
 
     public void OnPointerClick(PointerEventData eventData)
@@ -106,4 +132,37 @@ public class Interactable : MonoBehaviour,  IPointerClickHandler, IDragHandler, 
     {
         IsDragging = false;
     }
+
+    public void OnCreationStart(Transform parent, Vector3 startPosition)
+    {
+        var tr = GetTransform();
+
+        tr.SetParent(parent);
+        tr.position = startPosition;
+        SetScale(0);
+        SetActive(false);
+    }
+
+    public void OnCreationFinish()
+    {
+        if (spriteRenderer.size.x > .3)
+            SetActive(true);
+        else
+            Destroy(gameObject);
+    }
+
+    public void OnCreationUpdate(Vector3 newPosition, Vector3 startPosition)
+    {
+        var dir = newPosition - startPosition;
+        var center = (startPosition + newPosition) * 0.5f;
+
+        var position = center;
+        var rotation = Quaternion.FromToRotation(Vector3.right, dir);
+        
+        transform.SetPositionAndRotation(position, rotation);
+        SetScale(dir.magnitude);
+    }
+
+    public Transform GetTransform() => transform;
+    public Interactable GetInteractable() => this;
 }
